d678f6ee569faa159b251fdb11eacc68de16d878
yuri <marinhoy870@gmail.com>
2026-05-22 10:29:15 -0300

    alterando a classe Frigobar
    Exclui o repositorio de ItensFrigobar, que servia como inventario de cada frigobar, e adicionei um arrayList especifico dentro da classe Frigobar

--- src/models/Frigobar.java
+++ src/models/Frigobar.java
@@ -1,37 +1,43 @@
 package models;
-import dados.RepositorioItensFrigobar;
+
+import java.util.ArrayList;
 
 //Temos que lembrar de criar uma função pra reconhecer quando um item foi comprado
 public class Frigobar {
-    private RepositorioItensFrigobar inventarioFrigobar;
+    private ArrayList<ItemFrigobar> inventarioFrigobar;
     private final int capacidadeMaxima; // a capacidade nao pode ser alterada
 
     // construtores
     public Frigobar(int capacidadeMaxima) {
-        this.inventarioFrigobar = new RepositorioItensFrigobar(capacidadeMaxima);
+        this.inventarioFrigobar = new ArrayList<>(capacidadeMaxima);
         this.capacidadeMaxima = capacidadeMaxima;
     }
 
     // Getters e Setters
-    public RepositorioItensFrigobar getInventarioFrigobar() {
+    public ArrayList<ItemFrigobar> getInventarioFrigobar() {
         return inventarioFrigobar;
     }  
     public int getCapacidadeMaxima() {
         return capacidadeMaxima;
     }
 
-    public void setInventarioFrigobar(RepositorioItensFrigobar inventarioFrigobar) {
+    public void setInventarioFrigobar(ArrayList<ItemFrigobar> inventarioFrigobar) {
         this.inventarioFrigobar = inventarioFrigobar;
     }
     
     // Métodos para adicionar e remover itens do frigobar
     public void adicionarItem(ItemFrigobar item) {
-        inventarioFrigobar.adicionar(item);
+        if (inventarioFrigobar.size() < capacidadeMaxima ){
+            inventarioFrigobar.add(item);
+        } else {
+            // exceção de frigobar cheio
+        }
+        
     }
 
     public void removerItem(ItemFrigobar item) {
-        inventarioFrigobar.remover(item);
-        // caso o item seja removido, precisamos debitar da conta responsavel
+        inventarioFrigobar.remove(item);
+        // caso o item seja removido, precisamos adicionar na divida de Responsavel
     }
 
 }
