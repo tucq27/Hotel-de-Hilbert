71b27db0cd7c8c612b516774916bfbf97bb07406
Yuri Marinho <marinhoy870@gmail.com>
2026-05-30 00:59:02 -0300

    deletando projeto antigo
    o maven havia sido criado no vscode

--- hoteldehilbert/src/main/java/com/smarthotel/models/TipoRecibo.java
+++
@@ -1,5 +0,0 @@
-package com.smarthotel.models;
-
-public enum TipoRecibo {
-    DIARIA, SERVICO, FRIGOBAR;
-}
--- hoteldehilbert/src/main/java/com/smarthotel/models/exceptions/CINRException.java
+++
@@ -1,7 +0,0 @@
-package com.smarthotel.models.exceptions;
-
-public class CINRException extends Exception {
-    public CINRException() {
-        super("Check-out não realizado, pois não houve check-in!");
-    }
-}
