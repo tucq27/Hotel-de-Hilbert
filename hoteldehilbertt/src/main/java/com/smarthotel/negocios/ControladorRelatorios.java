029d9b788cf24793c80cd73e867bb03f358d4df2
yuri <marinhoy870@gmail.com>
2026-06-07 00:10:46 -0300

    criando controlador de relatorios e alertas

--- hoteldehilbertt/src/main/java/com/smarthotel/negocios/ControladorRelatorios.java
+++ hoteldehilbertt/src/main/java/com/smarthotel/negocios/ControladorRelatorios.java
@@ -1,19 +1,100 @@
 package com.smarthotel.negocios;
 
-import com.smarthotel.dados.RepoQuartos;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-public class ControladorRelatorios {
+import com.smarthotel.dados.RepoHospedagens;
+import com.smarthotel.models.Hospedagem;
+import com.smarthotel.models.StatusHospedagem;
+
+public class ControladorRelatorios implements IContRelatorios{
 
     public ControladorRelatorios() {
         
     }
 
-    // varre o repoQuartos e gera um relatório de ocupação, ou seja, quais quartos estão ocupados
-    public void gerarOcupacaoDiaria(RepoQuartos repositorioQuartos) {
-       
+    // verifica todas as hospedagens ativas agora
+    public ArrayList<Hospedagem> gerarOcupacaoDiaria(RepoHospedagens repositorioHospedagens) {
+        ArrayList<Hospedagem> hospedagensHotel = repositorioHospedagens.getObjetos();
+        ArrayList<Hospedagem> ocupacao = new ArrayList<>();
+        LocalDate hoje = LocalDate.now();
+
+        for (Hospedagem hosp : hospedagensHotel) {
+            LocalDateTime checkout = hosp.getHorarioCheckOut();
+
+            // verifica se a hospedagem esta ativa ou se o checkout ocorreu hoje
+            if ( (hosp.getStatus() == StatusHospedagem.ATIVA) || ((checkout != null) && (checkout.toLocalDate().isEqual(hoje)) ) ) {
+                ocupacao.add(hosp);
+            }
+        }
+
+        return ocupacao;
     }
 
-    public void gerarOcupacaoMensal(RepoQuartos repositorioQuartos) {
+    public ArrayList<Hospedagem> gerarOcupacaoMensal(RepoHospedagens repositorioHospedagens) {
+        ArrayList<Hospedagem> hospedagensHotel = repositorioHospedagens.getObjetos();
+        ArrayList<Hospedagem> ocupacao = new ArrayList<>();
 
+        LocalDate ultimoMes = LocalDate.now().minusMonths(1); // data atual -1 mes
+        int diasUltimoMes = ultimoMes.lengthOfMonth();
+
+        LocalDateTime inicioMes = ultimoMes.withDayOfMonth(1).atTime(0, 0, 0);
+        LocalDateTime fimMes = ultimoMes.withDayOfMonth(diasUltimoMes).atTime(23, 59, 59);
+        
+
+        for (Hospedagem hosp : hospedagensHotel) {
+            LocalDateTime checkout = hosp.getHorarioCheckOut();
+            LocalDateTime checkin = hosp.getHorarioCheckIn();
+
+            if (checkout == null) {
+                checkout = LocalDateTime.now();
+            }
+
+            // caso exista chekin, o quarto está sendo ocupado
+            if (checkin != null && checkout != null) {
+
+                // checkin feito até o fim do mes
+                // checkout feito a partir do inicio do mes
+                if ( !(checkin.isAfter(fimMes)) && !(checkout.isBefore(inicioMes)) ) {
+                    ocupacao.add(hosp);
+                }
+            }
+            
+            
+        }
+
+        return ocupacao;
+    }
+
+    ArrayList<Hospedagem> gerarAlertaSaidas(RepoHospedagens repositorioHospedagens) {
+        ArrayList<Hospedagem> hospedagensHotel = repositorioHospedagens.getObjetos();
+        ArrayList<Hospedagem> hospEncerradas = new ArrayList<>();
+
+        for (Hospedagem hosp : hospedagensHotel) {
+            if (hosp.getHorarioCheckOut() != null) {
+                hospEncerradas.add(hosp);
+            }
+        }
+
+        return hospEncerradas;
+    }
+
+    ArrayList<Hospedagem> gerarAlertaSaidaPendente(RepoHospedagens repositorioHospedagens) {
+        ArrayList<Hospedagem> hospedagensHotel = repositorioHospedagens.getObjetos();
+        ArrayList<Hospedagem> hospPendente = new ArrayList<>();
+        LocalDateTime agora = LocalDateTime.now();
+
+        for (Hospedagem hosp : hospedagensHotel) {
+            LocalDateTime horarioSaida = hosp.getHorarioSaida();
+            LocalDateTime checkout = hosp.getHorarioCheckOut();
+
+            // se já passou do horario de saida e o chekout não foi feito, adiciona na lista
+            if (agora.isAfter(horarioSaida) && checkout == null) {
+                hospPendente.add(hosp);
+            }
+        }
+
+        return hospPendente;
     }
 }
--- hoteldehilbertt/src/main/java/com/smarthotel/negocios/IContRelatorios.java
+++ hoteldehilbertt/src/main/java/com/smarthotel/negocios/IContRelatorios.java
@@ -1,5 +1,13 @@
 package com.smarthotel.negocios;
 
+import java.util.ArrayList;
+
+import com.smarthotel.dados.RepoHospedagens;
+import com.smarthotel.models.Hospedagem;
+
 public interface IContRelatorios {
+    ArrayList<Hospedagem> gerarOcupacaoDiaria(RepoHospedagens repositorioHospedagens);
+    ArrayList<Hospedagem> gerarOcupacaoMensal(RepoHospedagens repositorioHospedagens);
+
     
 }
