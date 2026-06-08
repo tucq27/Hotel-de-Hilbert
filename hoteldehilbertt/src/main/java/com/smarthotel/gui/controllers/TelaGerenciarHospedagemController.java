558467f0566bad3c642438c8cdbe10152e529d90
guxta1904 <gustavogomesarroz@gmail.com>
2026-06-07 22:27:57 -0300

    add mais telas

--- hoteldehilbertt/src/main/java/com/smarthotel/gui/controllers/TelaGerenciarHospedagemController.java
+++ hoteldehilbertt/src/main/java/com/smarthotel/gui/controllers/TelaGerenciarHospedagemController.java
@@ -1,94 +1,35 @@
 package com.smarthotel.gui.controllers;
 
-import com.smarthotel.negocios.ControladorHospedagens;
-import com.smarthotel.negocios.exceptions.*;
-
-
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
-public class TelaGerenciarHospedagemController extends TelaBuscarHospedagemController {
-
-    ControladorHospedagens controladorHospedagens = new ControladorHospedagens();
+public class TelaGerenciarHospedagemController {
 
     @FXML
     private Button btnVoltar;
-
-    @FXML
-    private void checkIn() {
-        try{
-            controladorHospedagens.checkIn(hospedagemSelecionada);
-
-            System.out.println("Check-in");
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Check-in Realizado");
-            alert.setContentText("Check-in realizado com sucesso para a hospedagem selecionada.");
-            alert.showAndWait();
-        } catch(CIJRException e) {
-            System.out.println(" - - - - Erro: " + e.getMessage());
-
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setContentText(e.getMessage());
-            alert.showAndWait();
-        } catch (CIFException e) {
-            System.out.println(" - - - - Erro: " + e.getMessage());
-
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setContentText(e.getMessage());
-            alert.showAndWait();
-        } catch (QIException e) {
-            System.out.println(" - - - - Erro: " + e.getMessage());
-
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setContentText(e.getMessage() +" Quarto: " + e.getQuarto().getId() + ", Status: " + e.getStatus());
-            alert.showAndWait();
-        }
-    }
-
-    @FXML
-    private void checkOut() {
-
-        try {
-            controladorHospedagens.checkOut(hospedagemSelecionada);
-
-            System.out.println("Check-out");
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Check-out Realizado");
-            alert.setContentText("Check-out realizado com sucesso para a hospedagem selecionada.");
-            alert.showAndWait();
-        } catch (CINRException e) {
-            System.out.println(" - - - - Erro: " + e.getMessage());
-
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setContentText(e.getMessage());
-            alert.showAndWait();
-        } catch (COJRException e) {
-            System.out.println(" - - - - Erro: " + e.getMessage());
-
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Erro");
-            alert.setContentText(e.getMessage());
-            alert.showAndWait();
-        }
-    }
 
     @FXML
     private ComboBox<String> cbTipoServico;
 
     @FXML
     public void initialize() {
-        cbTipoServico.getItems().add("Limpar Quarto");
-        cbTipoServico.getItems().add("Levar Comida");
-        cbTipoServico.getItems().add("Lavar Roupa");
+        if (cbTipoServico != null) {
+            cbTipoServico.getItems().add("Limpar Quarto");
+            cbTipoServico.getItems().add("Levar Comida");
+            cbTipoServico.getItems().add("Lavar Roupa");
+        }
+    }
+
+    @FXML
+    private void checkIn() {
+        System.out.println("Check-in");
+    }
+
+    @FXML
+    private void checkOut() {
+        System.out.println("Check-out");
     }
 
     @FXML
